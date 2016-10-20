b02a0596e6efbd6bb4c203c9de404b2d6fda573f
morrillo <gustavo.orrillo@moldeo.coop>
2016-10-20 19:49:09 -0300

    First commit

---
+++ __openerp__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Sucursales',
+    'category': 'Sales',
+    'version': '0.1',
+    'depends': ['base','account','account_voucher'],
+    'data': [
+	#'product_view.xml',
+	#'wizard/wizard_view.xml',
+    ],
+    'demo': [
+    ],
+    'qweb': [],
+    'installable': True,
+}
